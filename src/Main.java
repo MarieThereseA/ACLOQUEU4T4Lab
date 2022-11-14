7360de3faa3ef22a107eb78f591cd7a5629b6072
student <evelyns60@nycstudents.net>
2022-11-14 12:30:40 -0500

    hfd

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args){
-        String str1 = "apples and bananas";
-        while (str1.indexOf(" ") != -1) {
-            int index = str1.indexOf(" ");
-            str1 = str1.substring(0, index) + str1.substring(index + 1);
+        for (int j = 1; j <= 6; j++)
+        {
+            for (int i = 1; i <= j; i++)
+            {
+                System.out.print(" ");
+            }
+            for (int n = 1; n <= j; n++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println(str1);
+
+
+
+
+
 
     }
 }
