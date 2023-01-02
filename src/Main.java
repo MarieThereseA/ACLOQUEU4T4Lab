3babdd2490b7f84dac72052e5f1b105b10843055
MarieThereseA <ematetelove@gmail.com>
2023-01-02 12:02:47 -0500

    hgf

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args){
-        for (int j = 1; j <= 6; j++)
-        {
-            for (int i = 1; i <= j; i++)
-            {
-                System.out.print(" ");
+        int result = 0;
+        int m = 0;
+        while (m < 3) {
+            int k = 0;
+            while (k < 4) {
+                result++;
+                k++;
+                System.out.print("k: " + k);
+                System.out.println(", m: " + m);
             }
-            for (int n = 1; n <= j; n++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
+            m++;
         }
+        System.out.println("result: " + result);
+
+
 
 
 
